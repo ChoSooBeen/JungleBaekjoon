bd8d435558c7f8c396b3083ff753123efa0c7bd3
ChoSooBeen <cba0613060@gmail.com>
2023-04-12 16:52:40 +0900

    순열 함수 구현

--- Week01/10819.py
+++ Week01/10819.py
@@ -1,39 +1,33 @@
 import sys
-from itertools import permutations
 
 # 입력받기
 N = int(sys.stdin.readline())
 nums = list(map(int, sys.stdin.readline().split()))
 
-#순열 생성 -> 배열들의 원소들이 정렬되는 모든 경우를 구한다.
-# https://cijbest.tistory.com/12 참고
-nums_list = list(permutations(nums, N))
-
-# 재귀적으로 순열 생성하기
-# https://velog.io/@yeseolee/python으로-순열과-조합-직접-구현하기 참고
-# def permutation(arr, r):
-#     arr = sorted(arr)
-#     used = [0 for _ in range(len(arr))]
-
-#     def generate(chosen, used):
-#         if len(chosen) == r:
-#             print(chosen)
-#             return
-#         for i in range(len(arr)):
-#             if not used[i]:
-#                 chosen.append(arr[i])
-#                 used[i] = 1
-#                 generate(chosen, used)
-#                 used[i] = 0
-#                 chosen.pop()
-#     generate([], used)
-
+# 최종 결과값 저장할 변수
 result = 0
 
-for li in nums_list:
-   sum = 0
-   for j in range(N-1):
-      sum += abs(li[j]-li[j+1])
-   result = max(result, sum)
-
+# 사용여부를 저장할 배열
+used = [0] * N
+   
+# 순열 생성 함수
+def generate(chosen, used):
+   global result
+   if len(chosen) == N:
+      # 현재 리스트의 차이의 합을 저장할 변수
+      sum = 0
+      for i in range(N-1):
+         sum += abs(chosen[i]-chosen[i+1])
+      result = max(result, sum)
+      return 
+   
+   # 순열생성
+   for i in range(len(nums)):
+      if not used[i]:
+         chosen.append(nums[i])
+         used[i] = 1
+         generate(chosen, used)
+         used[i] = 0
+         chosen.pop()
+generate([], used)
 print(result)

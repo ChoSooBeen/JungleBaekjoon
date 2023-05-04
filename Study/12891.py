f18d972dbe318966b1613daad704b5ab808beee4
ChoSooBeen <cba0613060@gmail.com>
2023-05-04 09:28:29 +0900

    시간, 메모리 단축

--- Study/12891.py
+++ Study/12891.py
@@ -1,16 +1,39 @@
 import sys
-from collections import deque
 
 s, p = map(int, sys.stdin.readline().split())
 dna = list(sys.stdin.readline().strip())
 # A, C, G, T
 esssential = list(map(int, sys.stdin.readline().split()))
 
+def remove(st) :
+    global a, c, g, t
+    if dna[st] == 'A' :
+        a -= 1
+    elif dna[st] == 'C' :
+        c -= 1
+    elif dna[st] == 'G' :
+        g -= 1
+    elif dna[st] == 'T' :
+        t -= 1
+    return 0
+
+def add(e) :
+    global a, c, g, t
+    if dna[e] == 'A' :
+        a += 1
+    elif dna[e] == 'C' :
+        c += 1
+    elif dna[e] == 'G' :
+        g += 1
+    elif dna[e] == 'T' :
+        t += 1
+    return 0
+
 start = 0
 end = p-1
 count = 0
 # 현재 부분 문자열
-current = deque(dna[0:p])
+current = dna[0:p]
 # 각각의 원소 포함 개수
 a = current.count("A")
 c = current.count("C")
@@ -21,32 +44,11 @@ while end < s :
     # 만약에 모든 원소들의 조건을 충족했다면
     if a >= esssential[0] and c >= esssential[1] and g >= esssential[2] and t >= esssential[3] :
         count += 1
-    # 가장 첫번째 값 제거
-    current.popleft()
-    # 각각의 원소 개수 중 일치하는 것 빼기
-    if dna[start] == 'A' :
-        a -= 1
-    elif dna[start] == 'C' :
-        c -= 1
-    elif dna[start] == 'G' :
-        g -= 1
-    elif dna[start] == 'T' :
-        t -= 1
-    
+    remove(start)
     start += 1
     end += 1
-    # end가 끝에 도달했으므로
     if end == s :
         break
-    # 맨뒤에 문자 하나 추가
-    current.append(dna[end])
-    # 각각의 원소 개수 중 일치하는 것 추가
-    if dna[end] == 'A' :
-        a += 1
-    elif dna[end] == 'C' :
-        c += 1
-    elif dna[end] == 'G' :
-        g += 1
-    elif dna[end] == 'T' :
-        t += 1
+    add(end)
+    
 print(count)

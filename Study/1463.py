822d6f474141be30aa69d9eac045037363c442cd
ChoSooBeen <cba0613060@gmail.com>
2023-05-10 09:38:16 +0900

    2023.05.10

---
+++ Study/1463.py
@@ -0,0 +1,17 @@
+import sys
+
+N = int(sys.stdin.readline())
+dp = [0] *(N+1)
+if N > 1 :
+    dp[2] = 1
+    for i in range(3, N+1) :
+        if i % 6 == 0 :
+            dp[i] = min(dp[i//2], dp[i//3], dp[i-1]) + 1
+        elif i % 2 == 0 :
+            dp[i] = min(dp[i-1], dp[i//2]) + 1
+        elif i % 3 == 0 :
+            dp[i] = min(dp[i-1], dp[i//3]) + 1
+        else :
+            dp[i] = dp[i-1] + 1
+
+print(dp[N])

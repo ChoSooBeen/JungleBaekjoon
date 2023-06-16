1431bb1b6fb8e7f3baea84773250b89dda58a7d1
ChoSooBeen <cba0613060@gmail.com>
2023-06-17 00:29:25 +0900

    15655번 - N과과M(6)

--- Study/15655.py
+++ Study/15655.py
@@ -4,17 +4,19 @@ N, M = map(int, sys.stdin.readline().split())
 
 nums = list(map(int, sys.stdin.readline().split()))
 nums.sort()
+result = []
 
 def dfs(depth, idx) :
     if depth == M :
         print(*result)
-        result.pop()
         return
     for i in range(idx+1, N) :
         result.append(nums[i])
         dfs(depth+1, i)
+        result.pop()
     
 for i in range(N) :
-    result = []
+    result.clear()
     result.append(nums[i])
     dfs(1, i)
+    

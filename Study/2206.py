932be1b1122f0bb7e920fefe96d2dd83b1f777a8
ChoSooBeen <cba0613060@gmail.com>
2023-06-19 00:05:57 +0900

    벽 부수고 이동하기 - BFS

--- Study/2206.py
+++ Study/2206.py
@@ -5,31 +5,39 @@ N, M = map(int, sys.stdin.readline().split())
 
 graph = [list(sys.stdin.readline().strip()) for _ in range(N)]
 visited = [[0] * M for _ in range(N)]
+visited_broken = [[0] * M for _ in range(N)]
 
 def bfs() :
     queue = deque()
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
+    print_flag = True
     
     visited[0][0] = 1
+    visited_broken[0][0] = 1
     queue.append((0,0, False))
     
     while queue :
         cx, cy, flag = queue.popleft()
         if cx == N-1 and cy == M-1 :
+            result = max(visited[cx][cy], visited_broken[cx][cy])
+            print(result)
+            print_flag = False
             break
         for i in range(4) :
             nx, ny = cx + dx[i], cy + dy[i]
             if nx >= 0 and nx < N and ny >= 0 and ny < M :
-                if visited[nx][ny] == 0 :
-                    if graph[nx][ny] == '0' :
-                        queue.appendleft((nx, ny, flag))
+                if flag == True : #이미 한번 부쉈을 경우
+                    if graph[nx][ny] == '0' and visited_broken[nx][ny] == 0 :
+                        queue.append((nx, ny, flag))
+                        visited_broken[nx][ny] = visited_broken[cx][cy] + 1
+                else : #한번도 안 부순 경우
+                    if graph[nx][ny] == '0' and visited[nx][ny] == 0 :
+                        queue.append((nx, ny, flag))
                         visited[nx][ny] = visited[cx][cy] + 1
-                    elif graph[nx][ny] == '1' and not flag :
+                    elif graph[nx][ny] == '1' and visited_broken[nx][ny] == 0 :
                         queue.append((nx, ny, True))
-                        visited[nx][ny] = visited[cx][cy] + 1
-    if visited[N-1][M-1] == 0 :
+                        visited_broken[nx][ny] = visited[cx][cy] + 1
+    if print_flag :
         print(-1)
-    else :
-        print(visited[N-1][M-1])
 bfs()

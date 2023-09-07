5d5f3cadf0731554d1b57f606a0ed6f2c6431e01
ChoSooBeen <cba0613060@gmail.com>
2023-09-07 11:20:02 +0900

    흙길 보수하기 - greedy

--- Study/1911.py
+++ Study/1911.py
@@ -5,15 +5,19 @@ water = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 water.sort()
 
 count = 0
-current = -1
+current = -1 # 널빤지가 덮여지지 않는 시작점
 for x, y in water :
     if current < x : 
         current = x
-    length = y - current
+        
+    length = y - current # 덮어야할 물 웅덩이의 길이
+    need = 0 # 현재 필요한 널빤지의 개수
+    
     if length % l == 0 :
-        count += length // l
-        current += (length // l) * l
+        need = length // l
     else :
-        count += (length // l + 1)
-        current += (length // l + 1) * l
+        need = length // l + 1
+        
+    count += need
+    current += need * l
 print(count)
